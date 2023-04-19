d1f56e5626f3b7b0e065a3a9081bea11bcabc4cb
Chunyang Shen <cshen@cshen-macbookpro.local>
2023-04-19 16:09:15 -0700

    Update Logo

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -120,12 +120,12 @@ const Chat = () => {
   <div className="flex-1 p:2 sm:p-6 justify-between flex flex-col h-screen">
    <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
       <div className="relative flex items-center space-x-4">
-         <img src="https://ph-files.imgix.net/b739ac93-2899-4cc1-a893-40ea8afde77e.png" alt="" className="w-10 sm:w-16 h-10 sm:h-16 rounded-full"/>
+         <img src="https://i.imgur.com/W3vOpVE.png" alt="" className="w-10 sm:w-16 h-10 sm:h-16 rounded-full"/>
          <div className="flex flex-col leading-tight">
             <div className="text-2xl mt-1 flex items-center">
-               <span className="text-gray-700 mr-3">OpenAI</span>
+               <span className="text-gray-700 font-extrabold mr-3">Prometheus-GPT</span>
             </div>
-            <span className="text-md text-gray-600">GPT-3.5</span>
+            <span className="text-gray-600  text-xs text-right mr-3">Springsun Technology</span>
          </div>
       </div>
       <div className="flex items-center space-x-2">
--- src/components/login.tsx
+++ src/components/login.tsx
@@ -20,12 +20,12 @@ export default function Login() {
         <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
           <div className="max-w-md w-full space-y-8">
             <div>
-              <img className="mx-auto h-40 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
-              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              <img className="mx-auto h-70 w-auto" src="https://i.imgur.com/W3vOpVE.png" alt="Workflow" />
+              <p className="text-center text-3xl font-extrabold text-gray-900">
                 Prometheus-GPT
-              </h2>
-              <p className="mt-6 text-center text-sm text-gray-900">
-                Powered by SpringSun Technology
+              </p>
+              <p className="text-right text-sm text-gray-900 mr-24">
+                SpringSun Technology
               </p>
             </div>
 
