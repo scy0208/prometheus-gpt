4cd42f736a6ebe446073b8bfb79219251a2ce508
Chunyang Shen <cshen@cshen-macbookpro.local>
2023-04-18 22:34:48 -0700

    Change font in chat to text-lg

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -91,8 +91,8 @@ const Chat = () => {
         return (
           <div className="chat-message">
             <div className="flex items-end justify-end">
-                <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-                  <div><span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">{item}</span></div>
+                <div className="flex flex-col space-y-2 text-lg max-w-xs mx-2 order-1 items-end">
+                  <div><span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white">{item}</span></div>
                 </div>
                 <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-2"/>
             </div>
@@ -102,7 +102,7 @@ const Chat = () => {
         return (
           <div className="chat-message">
             <div className="flex items-end">
-              <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
+              <div className="flex flex-col space-y-2 text-lg max-w-xs mx-2 order-2 items-start">
                   <div><span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">{item}</span></div>
               </div>
               <img src="https://ph-files.imgix.net/b739ac93-2899-4cc1-a893-40ea8afde77e.png" alt="My profile" className="w-6 h-6 rounded-full order-1"/>
