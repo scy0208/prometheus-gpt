d90627b3bd505987807e70b42133883149245d39
Chunyang Shen <scy0208@gmail.com>
2023-10-01 21:33:13 -0700

    Fix

--- src/pages/api/email-analysis.ts
+++ src/pages/api/email-analysis.ts
@@ -168,7 +168,7 @@ export default async function POST(request: Request) {
   const res = await fetch('https://api.openai.com/v1/chat/completions', {
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer YOUR_API_KEY`,
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
       },
       method: 'POST',
       body: JSON.stringify(payload),
